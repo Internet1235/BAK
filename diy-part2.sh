a7d43dfe8e2556adf1ebf37255d9bb6b92bb0960
Internet1235 <135099493+Internet1235@users.noreply.github.com>
2024-03-09 22:24:04 +0800

    Initial commit

---
+++ diy-part2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+git clone https://github.com/Internet1235/openwrt-iptvhelper.git package/iptvhelper
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
+echo "# Put your custom commands here that should be executed once
+# the system init finished. By default this file does nothing.
+
+# 启动2.4g 和 5g 信号
+ip link set ra0 up
+ip link set rai0 up
+
+# 桥接网卡
+brctl addif br-lan ra0
+brctl addif br-lan rai0
+
+
+exit 0
+
+
+" > package/base-files/files/etc/rc.local
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
